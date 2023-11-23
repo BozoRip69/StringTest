8086b34c41cbae951660ae00f6aedbbb7f2239b4
tomekci34a@gmail.com <tomekci34a@gmail.com>
2023-11-23 13:26:23 +0100

    praca

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+//napisz kod ktory ze scannera wybiera jakas nazwe wycina 2 litery ze srodka i wrzuca je na poczatek
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-
-        ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj nazwe: ");
+        String nazwa = scanner.nextLine();
+        if(nazwa.length() >= 4){
+            int srodek = nazwa.length() / 2;
+            String wycieteLitery = nazwa.substring(srodek - 1, srodek + 1);
+            String nowaNazwa = wycieteLitery.concat(nazwa);
+            System.out.println("Nowa nazwa: " + nowaNazwa);
+        }   else {
+            System.out.println("Podaj dluzsza nazwe!");
+        }
     }
 }
